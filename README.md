3661247f9d92e8ec1d2b093d18bf0991f01185a2
EvanCharles16 <evancharles1609@gmail.com>
2020-04-04 13:57:17 +0700

    Project-Loop

---
+++ README.md
@@ -0,0 +1,16 @@
+# Project Loop
+
+## Create Factorial Using Loop
+For example : if variable num = 4 , then your program  should return (4 * 3 * 2 * 1)= 24 in new variable.
+
+## Create Reverse String
+For example : if variable yourName = "Amirul", then your program should return "lurimA" in new variable.
+
+## Create Reverse Array
+For example : if variable yourName = ['apple','melon','mango'], then your program should return  "['mango','melon','apple']" in new variable.
+
+## Assign Grade Using for 
+For example : When grade = 50 , return 'D', 70 return 'C'.
+
+## Multiplier in for
+For example : Numb : 9 and index <= 2, result 9*0 , 9*1 , 9*2.
---
+++ js/script.js
@@ -0,0 +1,61 @@
+// //1. Create Factorial
+// let inputNumber = window.prompt('Please enter a number');
+// let total = 1;
+
+// for (n = 0; n < inputNumber ; n++){
+//     total = (inputNumber - n) * total
+
+//     hasil = inputNumber + '! = ' + total
+//     console.log(hasil);
+// }
+
+
+// //2. Create Reverse String
+// function reverseString(str){
+//     let splitSpring = str.split("")
+//     let reverseArray = splitSpring.reverse();
+//     let joinArray = reverseArray.join("")
+
+//     return joinArray;
+// }
+// reverseString("Evan Charles");
+
+
+// //3. Create Reverse Array
+// let buah = ['apple','melon','mango']
+// let reverseArray = buah.reverse();
+// console.log(reverseArray)
+
+
+// //4.  Assign Grade
+// function score(nilai) {
+//     if (nilai <= 60)
+//         return "E";
+//     else if (nilai <= 70) {
+//         return "D";
+//     } else if (nilai <= 80) {
+//         return "C";
+//     } else if (nilai <= 90) {
+//         return "B";
+//     } else if (nilai <= 100) {
+//         return "A";
+//     } else {
+//         return "Masukan nomor dari 0-100"
+//     }
+// }
+
+//     for (let i =0; i <= 100; i++) {
+//         const final = 80
+//         // const hasil = score (final)
+//         const hasil = score(i)
+//         // console.log(`nilai mu adalah ${final} scorenya adalah ${hasil}`);
+//         console.log(`nilai mu adalah ${i} scorenya adalah ${hasil}`)
+//     }
+
+
+    //5. Multiplier in for
+    // let multiplier = 9;
+    // for (let i = 0; i <= 5; i++) {
+    //     let result = multiplier * i;
+    //     console.log(multiplier + ' * ' + i + ' = ' + result);
+    // }
